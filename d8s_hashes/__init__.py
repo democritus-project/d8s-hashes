3f07b1238995483d8cb94383096bde913f5e0612
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-16 06:56:39 -0500

    Updating imports to reference new, d8s_... names

--- d8s_hashes/__init__.py
+++ d8s_hashes/__init__.py
@@ -4,9 +4,9 @@ except ImportError:
     from importlib_metadata import PackageNotFoundError, version  #type: ignore
 
 try:
-    __version__ = version('democritus_hashes')
+    __version__ = version('d8s_hashes')
 except PackageNotFoundError:
-    message = 'Unable to find a version number for "democritus_hashes". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-hashes/issues.'
+    message = 'Unable to find a version number for "d8s_hashes". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-hashes/issues.'
     print(message)
 
 __author__ = '''Floyd Hightower'''
